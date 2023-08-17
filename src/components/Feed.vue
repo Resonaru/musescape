e3fd9e3afc5389e48023df7a2d7dd6fb043dc87e
zenxha <komays5042@gmail.com>
2023-08-17 11:11:54 +0900

    add id to song objects

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -10,13 +10,16 @@
                 {
                     artist: "Yorushika",
                     title: "Eat the Wind",
-                    img: "https://i.scdn.co/image/ab67616d0000b2734e25902b79f09e0c71f6c552"
+                    img: "https://i.scdn.co/image/ab67616d0000b2734e25902b79f09e0c71f6c552",
+                    timestamp: new Date('August 15, 2023 03:24:00'),
+                    ID: "0001"
             },
                 {
                     title: "Idol",
                     artist: "YOASOBI",
                     img: "https://i.scdn.co/image/ab67616d0000b273b7d6ca50bf766ad72226290c",
-                    timestamp: new Date('August 15, 2023 03:24:00')
+                    timestamp: new Date('August 15, 2023 03:24:00'),
+                    ID: "0002"
                 }
         ]
             }
