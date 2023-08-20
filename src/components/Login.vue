68d0051198935cb404069057d2d7fa2fb0084642
ajarean <117534525+ajarean@users.noreply.github.com>
2023-08-20 22:22:43 +0900

    basic login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,24 @@
 <template>
-    <div class="login-page">
-    </div>
-  </template>
+  <v-card class="mx-auto" title="User Authentication">
+    <v-container>
+      <v-text-field v-model="username" label="Username" variant="underlined"></v-text-field>
+      <v-text-field v-model="password" label="Password" variant="underlined"></v-text-field>
+    </v-container>
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-btn>
+        Login
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
   
-  <script>
-  </script>
-  
+<script>
+  export default {
+    data: () => ({
+      username: null,
+      password: null,
+    }),
+  }
+</script>
