4d79d0bd837fb94128d648d4cc03628af7a59346
zenxha <komays5042@gmail.com>
2023-08-22 12:47:08 +0900

    display discussions on song page (very ugly)

--- src/views/SongView.vue
+++ src/views/SongView.vue
@@ -1,58 +1,50 @@
 <template>
-    <div class="song-view">
-    <v-container>
-      <v-row>
-        <v-col cols="12" md="4">
-          <template v-if="songData">
-            <v-img width="600" :src="songData.img" alt="Album Cover"></v-img>
-          </template>
-        </v-col>
-        <v-col cols="12" md="8">
-          <v-row>
-            <v-col cols="12">
-              <template v-if="songData">
-                <h1 class="song-title">{{ songData.title }}</h1>
-                <h2 class="song-artist">{{ songData.artist.name }}</h2>
-                <h2>ID: {{ id }}</h2>
+      <!-- Right side (Song Card) -->
+      <!-- <v-navigation-drawer location="right" style="background-color: black;" :width="325"> -->
+        <v-row>
+          <v-col col="12">
+            <template v-if="loading">
+          Loading song...
+          
+        </template>
+        <template v-else>
+          <v-card cover class="song-card rounded-xl">
+            <v-img class="align-content-center rounded-xl" :src="songData.img" alt="Album Cover"></v-img>
+            <div class="song-details">
+              <h1 class="song-title">{{ songData.title }}</h1>
+              <h2 class="song-artist">{{ songData.artist.name }}</h2>
+              <h2>ID: {{ id }}</h2>
+            </div>
+          </v-card>
+        </template>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col col="12">
+            <div>
+          <h1>Discussions</h1>
+              <template v-if="noPosts">
+                No posts yet
               </template>
               <template v-else>
-                <h1>Loading song data...</h1>
+                <template v-for="post in posts">
+                  <h1>{{ post.title }}</h1>
+                  <p>{{ post.content }}</p>
+                </template>
               </template>
-            </v-col>
-            <v-col cols="12">
-              <v-card class="lyrics">
-                <v-card-title>
-                  <h2 class="lyrics-title">Lyrics</h2>
-                </v-card-title>
-                <v-card-text>
-                  <!-- Collapsible lyrics section content -->
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
+        </div>
+          </v-col>
+        </v-row>
+
+
+
+
+      <!-- </v-navigation-drawer> -->
+
+      <!-- Center side (Discussions) -->
       
-      <v-row>
-        <v-col cols="12">
-          <v-card class="discussions">
-            <v-card-title>
-              <h2 class="discussions-title">Discussions</h2>
-            </v-card-title>
-            <v-card-text>
-              <!-- List of discussion previews -->
-              <!-- <v-list>
-                <v-list-item v-for="discussion in discussions" :key="discussion.id">
-                  <v-list-item-title class="discussion-title">{{ discussion.title }}</v-list-item-title>
-                  <v-list-item-subtitle class="discussion-summary">{{ discussion.summary }}</v-list-item-subtitle>
-                </v-list-item>
-              </v-list> -->
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+
 </template>
 
 <script>
@@ -76,7 +68,10 @@ export default {
   props: ['id'], // Access the song ID from the route parameter
   data() {
     return {
-      songData: null, // Loading screen rendered
+      songData: null,
+      posts: [],
+      loading: true, // Loading screen renderred
+      noPosts: null
     };
   },
 
@@ -96,13 +91,27 @@ export default {
           console.log(`Successfully fetched song ${this.songData.title}`)
 
           console.log("Now fetching artist...");
-          const docSnap2 = await getDoc(song.artist) // get artist object from firestore from reference
-          const artist = docSnap2.data();
+          const artist = (await getDoc(song.artist)).data() // get artist object from firestore from reference
           console.log(`Successfully fetched artist ${artist.name}`)
           this.songData.artist = {
             name: artist.name,
             img: artist.img
           }
+          console.log("Fetching discussions")
+          song.posts.forEach(async postReference => {
+            try {
+              const postObject = (await getDoc(postReference)).data();
+              console.log(postObject)
+              this.posts.push({
+              title: postObject.title,
+              author: (await getDoc(postObject.author)).data().username,
+              content: postObject.content
+            })
+            } catch(e) {
+              this.noPosts = true;
+            }
+          })
+          this.loading = false;
   
 
 
@@ -122,5 +131,19 @@ export default {
 </script>
 
 <style>
-/* Add your CSS for song page styling */
+
+.song-img {
+  
+}
+.song-title {
+  color: #FFFFFF
+}
+.song-artist {
+  color: #959090
+}
+
+.song-card {
+
+  background-color: #423A42;
+}
 </style>
