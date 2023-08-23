be83485037497af4ca86b6007a79250844699462
ajarean <117534525+ajarean@users.noreply.github.com>
2023-08-24 00:03:19 +0900

    more auth zzz

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,8 +1,8 @@
 import { defineStore } from 'pinia';
 import {auth} from 'firebase';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
-export const authStore = defineStore('authStore', () =>{
+export const useAuthStore = defineStore('authStore', () =>{
     const registerUser = (credentials) => {
         createUserWithEmailAndPassword(auth, credentials.email, credentials.password)
             .then((userCredential) => {
@@ -13,8 +13,20 @@ export const authStore = defineStore('authStore', () =>{
                 console.log(error.message);
             });
     };
+
+    const loginUser = (credentials) => {
+        signInWithEmailAndPassword(auth, credentials.email, credentials.password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    };
     return{
-        registerUser
+        registerUser,
+        loginUser
     };
 });
 
