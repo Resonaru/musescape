feace6902226824d9e1a0c994015edec91a6baa5
GLKumfert <gkumf001@ucr.edu>
2023-08-23 12:28:09 +0900

    fix search

--- src/components/SearchResultsDropdown.vue
+++ src/components/SearchResultsDropdown.vue
@@ -1,21 +1,10 @@
-<!-- <template>
+<template>
     <div class="search-results-dropdown">
       <ul>
         <li v-for="(result, index) in results" :key="index">
           <search-results-item :info="result" />
         </li>
       </ul>
-    </div>
-  </template> -->
-  <template>
-    <div class="autocomplete-container">
-      <v-autocomplete
-        :items="results"
-        label="Search"
-        auto-select-first
-        variant="outlined"
-        class="custom-autocomplete"
-      ></v-autocomplete>
     </div>
   </template>
   
@@ -33,15 +22,6 @@
   </script>
   
   <style>
-  .autocomplete-container {
-    position: relative;
-  }
-  
-  .custom-autocomplete__content {
-    position: absolute;
-    top: 100%;
-    left: 0;
-    z-index: 1000; /* Adjust the z-index value as needed */
-  }
+  /* Add your custom styles here */
   </style>
   
